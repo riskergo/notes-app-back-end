5534c6886ca585e9eaa2614a7146d08ee2bb1983
riskergo <riski.sputra199@gmail.com>
2025-03-11 14:44:13 +0700

    mengubah huruf kapital

--- src/handler.js
+++ src/handler.js
@@ -17,7 +17,7 @@ const addNoteHandler = (request, h) => {
   if (isSuccess) {
     const response = h.response({
       status: 'success',
-      message: 'Catatan Berhasil ditambahkan',
+      message: 'Catatan berhasil ditambahkan',
       data: {
         noteId: id,
       },
